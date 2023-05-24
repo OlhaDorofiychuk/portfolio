aa8fb0e82ff8204bad2b1d6a1405ae4d0ee40030
OlhaDorofiychuk <olha.d@yahoo.com>
2023-05-24 20:08:21 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
+import {useState} from "react"
+import useMediaQuerry from "./hooks/useMediaQuerry"
+import Navbar from "./Navbar";
 function App() {
+  const [selectedPage, setSelectedPage]= useState('home');
+  const isAboveMediumScreens= useMediaQuerry("(min-width: 1060px)");
+
   return (
-    <div className="App">
-Hello
+   
+    <div className="app bg-deep-blue">
+<Navbar 
+selectedPage={selectedPage} 
+setSelectedPage={setSelectedPage}
+/>
     </div>
   );
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll"
+import useMediaQuerry from "./hooks/useMediaQuerry";
+
+const Link = ({page, selectedPage,setSelectedPage})=>{
+    const lowerCasePage = page.toLowerCase();
+    return(
+        <AnchorLink className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
+        hoover:text-yellow transition-500`}
+        href={`#${lowerCasePage}`}
+        onClick={()=> setSelectedPage(lowerCasePage)}
+        > 
+        {page}
+        </AnchorLink>
+    )
+}
+const  Navbar =({isTopOfPage,selectedPage, setSelectedPage})=>{
+    const [isMenuToggled, setIsMenuToggled]= useState(false);
+    const isAboveSmallScren= useMediaQuerry("(min-width:760px");
+    const navbarBackground = isTopOfPage ? "" : "bg-red";
+
+    return (
+        <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
+<div className="flex items-center justify-between mx-auto w-5/6">
+    <h4 className="font-playfair text-3xl font-bold">OD
+    </h4>
+    {/*desktop nav*/}
+    {isAboveSmallScren ? (
+        <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+<Link 
+page="Home"
+selectedPage={selectedPage}
+setSelectedPage={selectedPage}
+/>
+<Link 
+page="Skils"
+selectedPage={selectedPage}
+setSelectedPage={selectedPage}
+/>
+<Link 
+page="Projects"
+selectedPage={selectedPage}
+setSelectedPage={selectedPage}
+/>
+<Link 
+page="Contacs"
+selectedPage={selectedPage}
+setSelectedPage={selectedPage}
+/>
+ </div>
+    ) :(<button>
+        <img alt="menu-icon" src="../assets/menu-icon.svg"
+        className="rounded-full bg-red p-2"
+        onClick={()=>setIsMenuToggled(!isMenuToggled)}/>
+    </button>)}
+
+     {/* MOBILE MENU POPUP */}
+     {!isAboveSmallScren&& isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img alt="close-icon" src="../assets/close-icon.svg" />
+              </button>
+            </div>
+{/* MENU ITEMS */}
+<div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Skills"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              
+              <Link
+                page="Contact"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+    
+    
