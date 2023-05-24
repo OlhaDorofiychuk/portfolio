b79503711920ccef836d4ff0e8f65e3a3a09d250
OlhaDorofiychuk <olha.d@yahoo.com>
2023-05-24 22:09:44 +0100

    landing added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import useMediaQuerry from "./hooks/useMediaQuerry"
 import Navbar from "./Navbar";
+import DotGroup from "./DotGroup";
+import Landing from "./Landing";
+import { motion } from "framer-motion";
+
 function App() {
   const [selectedPage, setSelectedPage]= useState('home');
+  const[isTopOfPage, setIsTopOfPage]=useState(true)
   const isAboveMediumScreens= useMediaQuerry("(min-width: 1060px)");
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
 
   return (
-   
     <div className="app bg-deep-blue">
-<Navbar 
-selectedPage={selectedPage} 
-setSelectedPage={setSelectedPage}
-/>
-    </div>
-  );
-}
-
+    <Navbar 
+      selectedPage={selectedPage} 
+      setSelectedPage={setSelectedPage}
+      isTopOfPage={isTopOfPage}
+      
+    />
+  <div className="w-5/6 mx-auto md:h-full">
+          {isAboveMediumScreens && (
+            <DotGroup
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+    )}
+  <motion.div
+            margin="0 0 -200px 0"
+            amount="all"
+            onViewportEnter={() => setSelectedPage("home")}
+          >
+            <Landing setSelectedPage={setSelectedPage} />
+          </motion.div>
+  </div>
+  </div>
+  
+)}
 export default App;
